1e8179a5bccc5203c429b6740534e66a73d65ad8
lucien <faurelucien@proton.me>
2025-02-04 21:36:47 +0100

    initial commit, very basic nix configuration

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "nix laptop flake";
+  inputs = {
+    # NixOS official unstable branch
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # "nixos" should be the hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # import stuff here
+        ./configuration.nix
+      ];
+    };
+
+  };
+  
+}
