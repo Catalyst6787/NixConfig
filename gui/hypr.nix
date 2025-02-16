f090251a133cb6406cac77ec1874edfeffd3d8c7
lucien <faurelucien@proton.me>
2025-02-16 20:02:53 +0100

    added hyprcursor to gui

--- gui/hypr.nix
+++ gui/hypr.nix
@@ -3,8 +3,10 @@
 {
   programs.hyprland.enable = true;
   environment.systemPackages = [
+    pkgs.banana-cursor
     pkgs.kitty
     pkgs.librewolf
+    pkgs.hyprcursor
   ];
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
